4a0033d8d355544526b88ffa13ec6e8658e61c67
javad soltani <j.soltani.it@gmail.com>
2018-07-15 17:47:24 +0430

    added use to sample pages

--- sample/test-callback.php
+++ sample/test-callback.php
@@ -1,6 +1,8 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using
 
+use \src\jsBankParsian as jsParsianBank;
+
 $Token = $_REQUEST['Token'] ?? '';
 $Status = $_REQUEST['status'] ?? -1;
 $TerminalNo = $_REQUEST['TerminalNo'] ?? '';
@@ -9,7 +11,7 @@ $TspToken = $_REQUEST['TspToken'] ?? '';
 $HashCardNumber = $_REQUEST['HashCardNumber'] ?? '';
 $Amount = $_REQUEST['Amount'] ?? 0;
 
-$getCallback = \src\jsBankParsian::callback($RRN, $Token, $Status);
+$getCallback = jsParsianBank::callback($RRN, $Token, $Status);
 $Code    = $getCallback->responseCode ?? -1;
 $Message = $getCallback->responseMessage ?? 'Error';
 
--- sample/test-pay.php
+++ sample/test-pay.php
@@ -1,12 +1,13 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using
 
-// \src\jsBankParsian::setParsianPin('scsdsdfbdsthsgfnfgndg');
-// \src\jsBankParsian::setCallBackUrl('http://example.ir/callback');
+use \src\jsBankParsian as jsParsianBank;
+// jsParsianBank::setParsianPin('scsdsdfbdsthsgfnfgndg');
+// jsParsianBank::setCallBackUrl('http://example.ir/callback');
 
 $OrderId = time() . rand(000,999); // factor number
 $Amount  = 1000; // amount to pay
-$getPay  = \src\jsBankParsian::pay($OrderId, $Amount);
+$getPay  = jsParsianBank::pay($OrderId, $Amount);
 $getPay  = json_decode($getPay);
 $Code    = $getPay->responseCode ?? -1;
 $Message = $getPay->responseMessage ?? 'Error';
